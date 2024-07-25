f72d88108f1d925e25525cf350af1c6b32660591
Tanmay.jos <tanmayjoshi@MMMDVTANMAYJOS.local>
2024-07-25 15:52:10 +0530

    initial commit

---
+++ src/main/java/net/media/training/live/isp/SensingDoor.java
@@ -0,0 +1,45 @@
+package net.media.training.live.isp;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: goyalamit
+ * Date: Jul 11, 2011
+ * Time: 4:21:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SensingDoor implements Door{
+    private boolean isLocked;
+    private boolean isOpened;
+
+    public SensingDoor(Sensor sensor) {
+    sensor.register(this);
+    }
+
+    public void lock() {
+        this.isLocked = true;
+    }
+
+    public void unlock() {
+        this.isLocked = false;
+    }
+
+    public void open() {
+        if (!isLocked)
+            this.isOpened = true;
+    }
+
+    public void close() {
+        this.isOpened = false;
+    }
+
+    public void timeOutCallback() {
+      throw new NotImplementedException();
+    }
+
+    public void proximityCallback() {
+        this.isOpened = true;
+    }
+
+}
---
+++ src/main/java/net/media/training/live/srp/Employee.java
@@ -0,0 +1,64 @@
+package net.media.training.live.srp;
+
+
+public class Employee {
+    private int empId;
+    private static int TOTAL_LEAVES_ALLOWED = 30;
+
+    private double monthlySalary;
+    private String name;
+    private String addressStreet;
+    private String addressCity;
+    private String addressCountry;
+    private int leavesTaken;
+    private int totalLeaveAllowed;
+    private int leaveTaken;
+    private String manager;
+    private int yearsInOrg;
+    private int thisYeard;
+    private int[] leavesLeftPreviously;
+
+
+    public Employee(int empId, double monthlySalary, String name, String addressStreet, String addressCity, String addressCountry, int leavesTaken, int[] leavesLeftPreviously) {
+        this.empId = empId;
+        this.monthlySalary = monthlySalary;
+        this.name = name;
+        this.addressStreet = addressStreet;
+        this.addressCity = addressCity;
+        this.addressCountry = addressCountry;
+        this.leavesTaken = leavesTaken;
+        this.leavesLeftPreviously = leavesLeftPreviously;
+        this.yearsInOrg = leavesLeftPreviously.length;
+    }
+
+    public Employee() {
+    }
+
+    public String toHtml() {
+        String str = "<div>" +
+                "<h1>Employee Info</h1>" +
+                "<div id='emp" + empId + "'>" +
+                "<span>" + name + "</span>" +
+                "<div class='left'>" +
+                "<span>Leave Left :</span>" +
+                "<span>Annual Salary:</span>" +
+                "<span>Manager:</span>" +
+                "<span>Reimbursable Leave:</span>" +
+                "</div>";
+        str += "<div class='right'><span>" + (totalLeaveAllowed - leaveTaken) + "</span>";
+        str += "<span>" + Math.round(monthlySalary * 12) + "</span>";
+        if (manager != null) str += "<span>" + manager + "</span>";
+        else str += "<span>None</span>";
+        int years = 3;
+        if (yearsInOrg < 3) {
+            years = yearsInOrg;
+        }
+        int totalLeaveLeftPreviously = 0;
+        for (int i = 0; i < years; i++) {
+            totalLeaveLeftPreviously += leavesLeftPreviously[yearsInOrg-i-1];
+        }
+        str += "<span>" + totalLeaveLeftPreviously + "</span>";
+        return str + "</div> </div>";
+    }
+    //other method related to customer
+}
